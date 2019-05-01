6859f843b16b4b8d64fe49838895e43f1a4c460c
Meet Parikh <m.parikh@reply.com>
2019-05-01 14:44:43 +0100

    Creating empty gitignore files

--- GitEmptyDirs.py
+++ GitEmptyDirs.py
@@ -4,13 +4,20 @@ from shutil import copy2
 
 
 def main():
-    dirName = argv[1]
+    dirName = argv[1] if len(argv) > 1 else os.getcwd()
     listOfEmptyDirs = [dirpath for (dirpath, dirnames, filenames) in os.walk(
         dirName) if len(dirnames) == 0 and len(filenames) == 0]
 
-    for elem in listOfEmptyDirs:
-        print(elem)
-        copy2(".gitignore", elem)
+    if len(listOfEmptyDirs) == 0:
+        print("No empty directories found.")
+    else:
+        print("Found following empty directories")
+        for elem in listOfEmptyDirs:
+            print(elem)
+            if len(argv) > 2:
+                copy2(argv[2], elem)
+            else:
+                open(os.path.join(elem, ".gitignore"), 'a').close()
 
 
 if __name__ == '__main__':
